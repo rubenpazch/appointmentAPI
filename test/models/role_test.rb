3fe79c1660a224ffa9e7b27c483ce8870a8bb89a
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-01-15 09:49:30 -0500

    Add test cases user and person

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class RoleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'role with a valid name should be valid' do
+    role = Role.new(name: 'patient', code: 'A')
+    assert role.valid?
+  end
+  test 'role with a invalid name should be invalid' do
+    role = Role.new(name: '', code: 'A')
+    assert_not role.valid?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
   test 'user with a valid email should be valid' do
     role = Role.first
-    person = Person.create! fistName: Faker::Name.first_name,
+    person = Person.create! firstName: Faker::Name.first_name,
                             lastName: Faker::Name.last_name,
                             documentId: Faker::IDNumber.chilean_id,
                             phone: Faker::PhoneNumber.cell_phone_in_e164,
@@ -16,7 +16,7 @@ class UserTest < ActiveSupport::TestCase
   end
   test 'user with duplicate email should be invalid' do
     role = Role.first
-    person = Person.create! fistName: Faker::Name.first_name,
+    person = Person.create! firstName: Faker::Name.first_name,
                             lastName: Faker::Name.last_name,
                             documentId: Faker::IDNumber.chilean_id,
                             phone: Faker::PhoneNumber.cell_phone_in_e164,
@@ -30,7 +30,7 @@ class UserTest < ActiveSupport::TestCase
   end
   test 'user with a invalid email should be invalid' do
     role = Role.first
-    person = Person.create! fistName: Faker::Name.first_name,
+    person = Person.create! firstName: Faker::Name.first_name,
                             lastName: Faker::Name.last_name,
                             documentId: Faker::IDNumber.chilean_id,
                             phone: Faker::PhoneNumber.cell_phone_in_e164,
