3467a5e5782a446cafedb37148fc0591072573a4
ruben paz chuspe <rubenpazchuspe@outlook.com>
2021-01-14 21:46:34 -0500

    add user migration

---
+++ db/migrate/20210115023942_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.index :email, unique: true
+      t.string :password_digest, null: false
+      t.string :username, null: false
+      t.index :username, unique: true
+
+      t.timestamps
+    end
+  end
+end
